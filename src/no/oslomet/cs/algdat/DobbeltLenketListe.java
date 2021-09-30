16daa8164b784ecb14ed19bb6fda69b7e6a0951c
André Brodtkorb <andre.brodtkorb@oslomet.no>
2021-09-30 10:40:08 +0200

    Updated and tested code after lecture

--- src/no/oslomet/cs/algdat/DobbeltLenketListe.java
+++ src/no/oslomet/cs/algdat/DobbeltLenketListe.java
@@ -2,6 +2,30 @@ package no.oslomet.cs.algdat;
 
 public class DobbeltLenketListe {
 
+    public static void main(String[] args) {
+        DobbeltLenketListe values = new DobbeltLenketListe();
+
+        values.add_first(3);
+        values.print();
+        //Forventer utskrift 3,
+
+        values.add_first(1);
+        values.print();
+        //Forventer utskrift 1, 3,
+
+        values.add_first(7);
+        values.print();
+        //Forventer utskrift 7, 1, 3
+
+        values.add_last(9);
+        values.print();
+        //Forventer utskrift 7, 1, 3, 9
+
+        values.insert(1, 8);
+        values.print();
+        //Forventer utskrift 7, 8, 1, 3, 9
+    }
+
     /**
      * Representasjon av en node i en dobbelt lenket
      * liste
@@ -39,6 +63,25 @@ public class DobbeltLenketListe {
             this.num_values = 0;
     }
 
+    public void print() {
+        //Alternativ 1: skriv som for-løkke
+        //[0, num_values)
+        Node p = head;
+        for (int i=0; i<num_values; ++i) {
+            System.out.print(p.value + ", ");
+            p = p.next;
+        }
+        System.out.println();
+
+        //Alternativ 2: skriv som while-løkke
+        Node q = head;
+        while (q != null) {
+            System.out.print(q.value + ", ");
+            q = q.next;
+        }
+        System.out.println();
+    }
+
     /**
      * Add funksjon som legger til ved hode-enden
      * av den dobbelt lenkede listen. Synonymer er:
@@ -53,8 +96,18 @@ public class DobbeltLenketListe {
         //head == null
         //tail == null
         if ( (head == null) && (tail == null) ) {
+      //if (num_values == 0) {
             head = p;
             tail = p;
+            num_values += 1;
+        }
+        //Generelt tilfelle: vi har noder allerede
+        //i listen vår
+        else {
+            Node q = head;
+            head = p;
+            q.prev = p;
+            p.next = q;
             num_values += 1;
         }
 
@@ -65,14 +118,55 @@ public class DobbeltLenketListe {
      * Add funksjon som legger til ved hale-enden
      * Synonymer: back, tail, ...
      */
-    void add_last() {
+    int add_last(int value) {
+        Node q = new Node(value);
 
+        if ( (head == null) && (tail == null) ) {
+            head = q;
+            tail = q;
+            num_values += 1;
+        }
+        else {
+            Node p = tail;
+            tail = q;
+            q.prev = p;
+            p.next = q;
+            num_values += 1;
+        }
+
+        return num_values;
     }
 
     /**
      * insert funksjon
+     * Legger til ny node med index index
      */
-    void insert() {
-        //???
+    void insert(int index, int value) {
+        if (index == 0) {
+            //Spesialtilfelle!
+            add_first(value);
+        }
+        else if (index == num_values-1) {
+            //spesialtilfelle - add_last
+            add_last(value);
+        }
+        else {
+            Node q = new Node(value);
+
+            //Finn de nodene vi skal sette inn p mellom
+            //slik at vi får p - q - r i listen vår
+            Node r = head;
+            for (int i = 0; i < index; ++i) {
+                r = r.next;
+            }
+            Node p = r.prev;
+
+            p.next = q;
+            q.next = r;
+            q.prev = p;
+            r.prev = q;
+
+            num_values += 1;
+        }
     }
 }
